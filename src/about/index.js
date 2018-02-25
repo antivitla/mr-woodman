08b769d2ca4a23a6eda8ae96e4f91bd3a003ce28
Виталий Кулешин <antivitla@gmail.com>
2018-02-25 21:12:59 +0300

    new texts

---
+++ src/about/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// CSS
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './index.css';
+import './navbar.css';
+import './section.css';
+import './welcome.css';
+import './profile-part.css';
+import './products.css';
+import './projects.css';
+import './diagonal.css';
+import './works.css';
+import './footer.css';
+
+// Javascript
+import 'mr-fontfit';
+import './works.js';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './about/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'about/index.js'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('about/index.css'),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+          publicPath: '../'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|woff|woff2|eot)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ]
+  }
+};
